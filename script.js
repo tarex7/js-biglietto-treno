371c1d37405f2b7eba614dd4ea8aa8fe1725fedc
tarex7 <tarex7@gmail.com>
2022-06-21 17:43:22 +0200

    aggiunta validazione input

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
-// Input Km da percorrere
-const kmTrip = parseInt(prompt("Chilometri da percorrere: "));
-//Input età del passeggero
-const userAge = parseInt(prompt("Età del passeggero: "));
-
 //UI elements
 const kmTripUI = document.getElementById("kmTrip");
 const userAgeUI = document.getElementById("userAge");
 const ticketPriceUI = document.getElementById("ticketPrice");
 
-//Prezzo biglietto
-let price = kmTrip * 0.21;
-let ticketPrice = price.toFixed(2);
+// Input Km da percorrere
+const kmTrip = parseInt(prompt("Chilometri da percorrere: "));
 
-//Prezzi scontati
-let priceUnder18 = ticketPrice - (ticketPrice * 0.2);
-let priceOver65 = ticketPrice - (ticketPrice * 0.4);
+//Input età del passeggero
+const userAge = parseInt(prompt("Età del passeggero: "));
 
-//Mostra nella UI
-kmTripUI.innerText = kmTrip;
-userAgeUI.innerText = userAge;
-ticketPriceUI.innerText = ticketPrice + " €";
+//Validazione input
+
+if (isNaN(kmTrip) || isNaN(userAge)) {
+
+  alert("Per favore inserire un numero");
+
+} else {
+
+  //Prezzo biglietto
+  let price = kmTrip * 0.21;
+  let ticketPrice = price.toFixed(2);
+
+  //Prezzi scontati
+  let discount20 = ticketPrice * 0.2;
+  let discount40 = ticketPrice * 0.4;
+
+  let priceUnder18 = ticketPrice -discount20;
+  let priceOver65 = ticketPrice - discount40;
 
 
-//Logica sconto biglietto
-//Under 18
-if (userAge < 18) {
+  //Mostra nella UI
+  kmTripUI.innerText = kmTrip;
+  userAgeUI.innerText = userAge;
+  ticketPriceUI.innerText = ticketPrice + " €";
 
-  ticketPriceUI.innerHTML =
-   `<del>${ticketPrice} €</del>
+  //Logica sconto biglietto
+  //Under 18
+  if (userAge < 18) {
+    ticketPriceUI.innerHTML = `<del>${ticketPrice} €</del>
    <br> 
-   <span id="discount-text">Applicato sconto under 18</span>
+   <span id="discount-text">Applicato sconto di ${discount20.toFixed(2)} € per under 18</span>
    <br>
    Totale ${priceUnder18.toFixed(2)} €`;
 
-//Over 65
-} else if (userAge > 65) {
-
-  ticketPriceUI.innerHTML = `<del>${ticketPrice} €</del>
+    //Over 65
+  } else if (userAge > 65) {
+    ticketPriceUI.innerHTML = `<del>${ticketPrice} €</del>
    <br> 
-   <span id="discount-text">Applicato sconto over 65</span>
+   <span id="discount-text">Applicato sconto di ${discount40.toFixed(2)} € per over 65</span>
    <br> 
    Totale ${priceOver65.toFixed(2)} €`;
+  }
 }
